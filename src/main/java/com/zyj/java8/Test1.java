6adefde1841151e0701a5bd10efb7d0b90db5d3c
pchad321 <hustzyj89@gmail.com>
2018-10-28 14:48:58 +0800

    test

--- src/main/java/com/zyj/java8/Test1.java
+++ src/main/java/com/zyj/java8/Test1.java
@@ -7,9 +7,12 @@ public class Test1 {
 
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
-        list.forEach(i -> System.out.println(i));
-        list.forEach(System.out::println);
+//        list.forEach(i -> System.out.println(i));
+//        list.forEach(System.out::println);
 
-        list.stream().map(item -> item * 2).forEach(System.out::println);
+        list.stream().map(item -> {
+            System.out.println("--------");
+            return item * 2;
+        }).forEach(System.out::println);
     }
 }
